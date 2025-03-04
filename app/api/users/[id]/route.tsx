b553e31b3029ce38790946913847a34a752059a0
Bivishan Sa <sapkotabivishan@gmail.com>
2025-03-04 12:37:49 +0545

    get,post & delete succeed

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -28,10 +28,22 @@ export async function GET(request: NextRequest, {params}:{params:{id:string}}) {
 // here is two parameters, one is request object, another is params object that contain our route parameters
 export async function PUT(request:NextRequest, {params}:{params:{id:string}}) {
     // to update the requested object of user we need to first, as long following,
-    // validate the request body
+    // validate the request body // const body = await request.json();
   const body =  await request.json();
-  if (!body.name) {
+  if (!body.name) { //checking if there is user's name is available or not means if  we  provide or not username to update the name object.
     return NextResponse.json({error:'name is required'}, {status:400});     
 }
-// fetch the user with given id & if doesn't exist, return 404
+// fetch the user with given id from db & if doesn't exist, return 404
+if (Number(params.id) > 10) {
+    return NextResponse.json({error: "user not found"}, {status:404});
+}
+// otherwise, update the user in the database and return the updated user object
+return NextResponse.json({id:1, name: body.name});
+}
+
+export function DELETE(request:NextRequest, {params}:{params:{id:string}}) {
+    const body = request.json();
+    if (Number(params.id) > 10) 
+        return NextResponse.json({error: "user not found"}, {status:404});
+    return NextResponse.json({}) // different application has different delete logic, or you can you can include the user  that was deleted in the body of the response.//empty object has returned
 }
