f9d75587aee0ea0b4be05b1dfde09f4302e98ecf
Bivishan Sa <sapkotabivishan@gmail.com>
2025-03-14 07:06:43 +0545

    session authentication just started

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,16 +1,31 @@
 // i create this component next to the root layout,, since, it's only one component here for all the pages, so, didn't create inside the components,,,
 // have to use components folder, only for reposity for the shared components.
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
 
 const NavBar = () => {
-  return (
-    <div className='flex space-x-8 bg-slate-300 p-3'>
-        <Link href='/' className='mr-5'>Home</Link>
-        <Link href='/users'>Users</Link>
-        <Link href='/api/auth/signin'>Login</Link>
-    </div>
-  )
-}
+  //destructing the usesession hook by grabbing status & data from usesession hook and data variable here can be rename to session
+  //we should make this client because with this hook, we access the context object that is passed using the session provider.
+  const { status, data: session } = useSession();
 
-export default NavBar
+  
+  //if the status is loading, then return loading text or loading spinner or return null;
+  return (
+    <div className="flex space-x-8 bg-slate-300 p-3">
+      <Link href="/" className="mr-5">
+        Home
+      </Link>
+      <Link href="/users">Users</Link>
+      {status === "loading" &&  <div>Loading...</div>};
+      {/* we give here ! cause when status === 'loading' we don't have user object, but when authenticated, we def. have a user object, that's why we are telling to TS compiler that we def. have a user */}
+      {status === "authenticated" && <div>{session.user!.name} </div>}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin">Login</Link>
+      )}{" "}
+    </div>
+  );
+};
+
+export default NavBar;
--- app/api/auth/token/route.ts
+++ app/api/auth/token/route.ts
@@ -1,7 +1,9 @@
+// this token valid for 30 days from the time it was created
+
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request : NextRequest) {
-  const token = await getToken({req: request})
+  const token = await getToken({req: request,secret: process.env.AUTH_SECRET}) //await this gettoken because it returns a promise object that we need to wait for
   return NextResponse.json(token);
 }
